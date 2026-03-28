ebd6377e1888eb5130c075e940e0b86e4a0dd4da
moranciejr <moranciejr@gmail.com>
2026-03-28 08:35:24 -0500

    Create stub for processing due plan notifications

---
+++ apps/web/src/app/api/utils/process-due-plan-notifications.js
@@ -0,0 +1,5 @@
+// Stub for processing due plan notifications
+export async function processDuePlanNotifications(sql) {
+  console.log('processDuePlanNotifications stub called');
+  return { processed: 0 };
+}
